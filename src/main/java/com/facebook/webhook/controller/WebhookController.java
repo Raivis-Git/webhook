8fa0b839c678d808a34f67a9ae20b13eabf22313
Raivis <raivukam2@gmail.com>
2024-10-04 18:08:01 +0300

    webhook init

---
+++ src/main/java/com/facebook/webhook/controller/WebhookController.java
@@ -0,0 +1,14 @@
+package com.facebook.webhook.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/facebook")
+public class WebhookController {
+
+    @GetMapping("/webhook")
+    public String hello() {
+        return "Hello, this is a secure HTTPS server!";
+    }
+
+}
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /bin/projects/facebook/webhook/
+/usr/bin/mvn spring-boot:run
