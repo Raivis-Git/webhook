4b37dc92f221d3d8d34201aa527f82cdb4cecc54
Raivis <raivukam2@gmail.com>
2024-10-04 19:03:38 +0300

    path api

--- src/main/java/com/facebook/webhook/controller/WebhookController.java
+++ src/main/java/com/facebook/webhook/controller/WebhookController.java
@@ -3,7 +3,7 @@ package com.facebook.webhook.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/facebook")
+@RestController("/api/facebook")
 public class WebhookController {
 
     @GetMapping("/webhook")
