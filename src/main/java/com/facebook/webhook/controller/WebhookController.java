d787934c26a4d3901c9208c07736bae51a623385
Raivis <raivukam2@gmail.com>
2024-10-05 05:07:48 +0300

    POST added

--- src/main/java/com/facebook/webhook/controller/WebhookController.java
+++ src/main/java/com/facebook/webhook/controller/WebhookController.java
@@ -6,27 +6,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("api/facebook")
+@RequestMapping("api/facebook/webhook")
 public class WebhookController {
 
     Logger LOGGER = LoggerFactory.getLogger(WebhookController.class);
     @Autowired
     ConfigLoader configLoader;
 
-    @GetMapping("/webhook/test")
+    @GetMapping("/test")
     public String test() {
         return "Hello, this is a secure HTTPS server!";
     }
 
-    @GetMapping("/webhook")
+    @GetMapping
     public ResponseEntity<?> webhookSubscribe(@RequestParam Map<String,String> allRequestParams, ModelMap model) {
 
         LOGGER.info("webhook GET received");
@@ -42,4 +39,14 @@ public class WebhookController {
             return ResponseEntity.status(403).build();
 
     }
+
+    @PostMapping
+    public ResponseEntity<?> webhookReceive(@RequestBody Object receivedData) {
+
+        LOGGER.info("webhook POST");
+        LOGGER.info(receivedData.toString());
+
+        return ResponseEntity.ok().build();
+    }
+
 }
