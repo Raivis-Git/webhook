e966b15ee3dd2797ef4ef5bcb985d6505db6bdf9
Raivis <raivukam2@gmail.com>
2024-11-15 16:26:01 +0200

    add endpoint for sendingMessages

--- src/main/java/com/facebook/webhook/controller/MainController.java
+++ src/main/java/com/facebook/webhook/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/facebook/send/")
+@RequestMapping("/api/facebook/send")
 public class MainController {
 
     @Autowired
